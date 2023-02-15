4ddaff96d7c08cdb4c9e746b3beabe859acf74a0
BastienGavioli <bastien.gavioli@etu.umontpellier.fr>
2023-02-15 09:26:15 +0100

    Correcting a bug in CommandeRemplacer

--- src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -16,8 +16,10 @@ public class CommandeRemplacer extends CommandeDocument{
         }
         int start = Integer.parseInt(parameters[1]);
         int end = Integer.parseInt(parameters[2]);
-        String remplacement = parameters[3];
-        this.document.remplacer(start, end, remplacement);
+        String replacement;
+        if (parameters.length == 3) replacement = "";
+        else replacement = parameters[3];
+        this.document.remplacer(start, end, replacement);
         super.executer();
     }
 }
