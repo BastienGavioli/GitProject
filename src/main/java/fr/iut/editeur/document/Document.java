571d3aa811d5c91d5b06cb3fd2249d5566e61a6f
BastienGavioli <bastien.gavioli@etu.umontpellier.fr>
2023-02-16 12:39:49 +0100

    Java doc

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -18,16 +18,30 @@ public class Document {
         this.texte = texte;
     }
 
+    /**
+     * Permet d'ajouter du texte à la fin de celui-ci s'il est déjà commencé
+     * @param texte
+     */
     public void ajouter(String texte) {
         this.texte += texte;
     }
 
+    /**
+     * Permet de remplacer la chaine de caractère située entre les index {@code start} et {@code end}.
+     * @param start index de départ
+     * @param end index de fin
+     */
     public void remplacer(int start, int end, String remplacement) {
         String leftPart = this.texte.substring(0, start);
         String rightPart = this.texte.substring(end);
         this.texte = leftPart + remplacement + rightPart;
     }
 
+    /**
+     * Permet de mettre en majuscule la chaine de caractère située entre les index {@code start} et {@code end}.
+     * @param start index de départ
+     * @param end index de fin
+     */
     public void majuscules(int start, int end) {
         String leftPart = this.texte.substring(0, start);
         String upperText = this.texte.substring(start, end).toUpperCase(new Locale("fr", "FR"));
@@ -35,10 +49,18 @@ public class Document {
         this.texte = leftPart + upperText + rightPart;
     }
 
+    /**
+     * Permet d'effacer la chaine de caractère située entre les index {@code start} et {@code end}.
+     * @param start index de départ
+     * @param end index de fin
+     */
     public void effacer(int start, int end) {
         remplacer(start, end, "");
     }
 
+    /**
+     * Efface tout le contenu du texte
+     */
     public void clear() {
         this.texte = "";
     }
