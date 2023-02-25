ea0ec5bc704aa7ad2eb529496e0f6427f6ce4248
BastienGavioli <bastien.gavioli@etu.umontpellier.fr>
2023-02-25 22:55:58 +0100

    refactor: Add unit tests

---
+++ src/test/java/fr/iutmontpellier/tests/DocumentTest.java
@@ -0,0 +1,36 @@
+package fr.iutmontpellier.tests;
+
+import fr.iut.editeur.document.Document;
+import junit.framework.TestCase;
+import org.junit.Before;
+public class DocumentTest extends TestCase {
+    private Document doc;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        doc = new Document();
+    }
+
+    public void test_document_ajouter_texte_a_texte_vide() {
+        doc.ajouter("Lorem");
+        assertEquals("Lorem", doc.getTexte());
+    }
+
+    public void test_document_ajouter_texte_a_texte_deja_rempli() {
+        doc.setTexte("Lorem");
+        doc.ajouter(" ipsus");
+        assertEquals("Lorem ipsus", doc.getTexte());
+    }
+
+    public void test_document_clear_texte_quand_texte_vide() {
+        doc.clear();
+        assertTrue(doc.getTexte().isEmpty());
+    }
+
+    public void test_document_clear_texte_quand_texte_deja_rempli() {
+        doc.setTexte("Lorem ipsus");
+        doc.clear();
+        assertTrue(doc.getTexte().isEmpty());
+    }
+}
